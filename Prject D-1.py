767b43e995c357d492aeacf88fee0f106a9eef74
GarterGod <149113964+GarterGod@users.noreply.github.com>
2024-10-22 18:05:15 -0400

    Update2 Prject D-1.py
    this improved it so the turns count properly and the code is a bit more readable

--- Prject D-1.py
+++ Prject D-1.py
@@ -33,58 +33,55 @@ Turns=0
 def Subtracting():
     global Net
     global Ball
-    global Turns
     global waw
     if waw<0:
         Ball=Ball-waw
         waw=waw*-1
         print(f"Adding {waw}")
+        print()
+        print(Ball)
+        print()
+        
     else:
         print(f"Subtracting {waw}")
         Ball=Ball-waw
-    Turns=Turns+1
-    print(Ball)
-    print()
-    
-   
+        print()
+        print(Ball)
+        print()
+         
 
 def Adding():
     global Net
     global Ball
-    global Turns
     global waw
     Roll()
     if waw<0:
         Ball=Ball+waw
         waw=waw*-1
         print(f"Subtracting {waw}")
+        print()
+        print(Ball)
+        print()
+       
     else:
         print(f"Adding {waw}")
         Ball=Ball+waw
-
-
-        Turns=Turns+1
+        print()
         print(Ball)
         print()
+      
+Maximum=0
+Minumum=0
 
-
-
-print()
-Maximum=int(input("Maximum   "))
-
-
-Minumum=int(input("Minumum   "))
-if Minumum>Maximum:
-     while Minumum>Maximum:
-          print("The Minumum can not be more than the Maximum   ")
-          Minumum=int(input("What is the minumum change you wish applied"))
-elif Maximum-Minumum<1:
-    print("You Need To Create A Bigger Diferrfence Or Else This Will Go On Forever")
-    Minumum=int(input("What is the minumum change you wish aplied    "))
-else:
-     print()
-          
+while Maximum-Minumum<1:
+    print()
+    Maximum=int(input("Maximum   "))
+    print()
+    Minumum=int(input("Minumum   "))
+print()  
 Net=int(input("What Number Do You Wish The Net To Be  "))
+print()
+print()
      
  
          
@@ -93,46 +90,54 @@ Net=int(input("What Number Do You Wish The Net To Be  "))
 
 
 def BallFinder (): 
-
+    global Ball
     Ballmin=0
     Ballmax=0
     while Ballmin==Ballmax:
-        global Ball
+      
         Roll()
         Ballmin=waw
         Roll()
         Ballmax=waw
-    if Ballmin>Ballmax: 
-        random_numbers = random.randint(Ballmax,Ballmin) 
-        Ball=random_numbers
-    elif Ballmax>Ballmin:
-        random_numbers = random.randint(Ballmin,Ballmax) 
-        Ball=random_numbers
+    Ball=Net
+
+    while Ball==Net:
+        if Ballmin>Ballmax: 
+            random_numbers = random.randint(Ballmax,Ballmin) 
+            Ball=random_numbers
+
+        elif Ballmax>Ballmin:
+            random_numbers = random.randint(Ballmin,Ballmax) 
+            Ball=random_numbers
+    
+    
 
 BallFinder() 
 
-#DDFFFFF
-Roll()
 
-if Ball==Net:
-    Ball=Ball+waw
-    print(Ball)
-else:
-    print(Ball)
+
+print(Ball)
+print()
 
 start_time = time.time()
 trutime=time.time()
 duration=10
 while trutime < start_time + duration: 
     trutime=time.time()  
-    Turns=Turns+1 
+    Roll()
+
     if Ball>Net:
         Subtracting()
+        Turns=Turns+1 
+
            
     elif Ball<Net:
         Adding()
+        Turns=Turns+1 
+
     else:
         trutime=time.time()+duration
+
         
 print(f"It Looped {Turns} Times")
 
